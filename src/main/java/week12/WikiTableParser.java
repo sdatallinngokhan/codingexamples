a1d7ae5e483bd5211581d2ea22a46996cc21c39d
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-02-10 15:57:01 +0200

    enhanced jsoup parser

--- src/main/java/week12/WikiTableParser.java
+++ src/main/java/week12/WikiTableParser.java
@@ -5,20 +5,36 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class WikiTableParser {
 
     public void parseTable() {
         try {
-            String url = "https://en.wikipedia.org/wiki/List_of_songs_recorded_by_Adele";
+            String url = "https://en.wikipedia.org/wiki/List_of_songs_recorded_by_Bruno_Mars";
             Document document = Jsoup.connect(url).get();
 
-            Elements elements = document.select("table.wikitable.sortable > tbody > tr > td");
+            Elements elements = document.select("table.wikitable.sortable > tbody > tr");
 
-            for (Element element : elements){
-                System.out.println(element.text());
+            StringBuilder sb = new StringBuilder();
+            for (Element element : elements) {
+                Elements column = element.select("td");
+                if (!column.isEmpty()){
+                    System.out.println(element.select("td").get(2).text());
+                }
+                sb.append(element.attr("title"));
+                sb.append("\n");
             }
 
+            File file = new File("/Users/gokhanpolat/Developer/tallinn/wikiToFile.txt");
+            FileWriter fileWriter = new FileWriter(file);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
 
+            printWriter.println(sb);
+
+            printWriter.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
